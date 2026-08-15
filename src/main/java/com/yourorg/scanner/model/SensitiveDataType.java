6f0dc1412766c88e883418509fe85987688427a9
rino_303 <singhritesh31836@gmail.com>
2026-08-16 00:13:00 +0530

    Expand sensitive data types

--- src/main/java/com/yourorg/scanner/model/SensitiveDataType.java
+++ src/main/java/com/yourorg/scanner/model/SensitiveDataType.java
@@ -1,8 +1,15 @@
 package com.yourorg.scanner.model;
 
-
 public enum SensitiveDataType {
     CARD_NUMBER,
+    AADHAAR_NUMBER,
     PAN_NUMBER,
-    AADHAAR_NUMBER
+    PASSPORT,
+    DRIVING_LICENCE,
+    VOTER_ID,
+    BANK_ACCOUNT,
+    IFSC,
+    UPI_ID,
+    PHONE_NUMBER,
+    EMAIL
 }
