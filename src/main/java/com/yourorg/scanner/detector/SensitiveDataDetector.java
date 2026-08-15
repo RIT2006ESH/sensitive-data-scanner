30da023e6819fffb0ceb70694cf414241f3a5b9b
rino_303 <singhritesh31836@gmail.com>
2026-08-15 10:12:18 +0530

    Update sensitive data detector

--- src/main/java/com/yourorg/scanner/detector/SensitiveDataDetector.java
+++ src/main/java/com/yourorg/scanner/detector/SensitiveDataDetector.java
@@ -3,9 +3,6 @@ package com.yourorg.scanner.detector;
 import com.yourorg.scanner.model.SensitiveDataType;
 
 import java.util.List;
-
-
-
 public interface SensitiveDataDetector {
 
     SensitiveDataType getType();
